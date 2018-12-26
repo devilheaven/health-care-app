f595acb2161883834de0ae5df99d5bc4dacb5084
thomastang303 <thomastang303@gmail.com>
2018-12-26 23:57:12 +0800

    [金諺]subscribe link

--- app/src/main/java/com/example/user/medical6/SubscribeActivity.java
+++ app/src/main/java/com/example/user/medical6/SubscribeActivity.java
@@ -2,6 +2,10 @@ package com.example.user.medical6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SubscribeActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class SubscribeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
+
+        WebView wvSubscribe = (WebView)findViewById(R.id.wvSubscribe);
+        WebSettings wsText = wvSubscribe.getSettings();
+        wsText.setJavaScriptEnabled(true);
+        wvSubscribe.setWebChromeClient(new WebChromeClient());
+        wvSubscribe.loadUrl("https://tlbinfo.cims.tw/pages/guan-yu-chan-pin");
     }
 }
