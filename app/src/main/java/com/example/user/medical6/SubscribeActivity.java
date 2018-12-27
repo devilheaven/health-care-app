c60340d73a3e75ab9ba1c9608267e1823e1c9472
devilheaven <f011368@gmail.com>
2018-12-27 21:28:25 +0800

    [冠志]subscribe webview has been fixed

--- app/src/main/java/com/example/user/medical6/SubscribeActivity.java
+++ app/src/main/java/com/example/user/medical6/SubscribeActivity.java
@@ -1,23 +1,34 @@
 package com.example.user.medical6;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class SubscribeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
-
-        WebView wvSubscribe = (WebView)findViewById(R.id.wvSubscribe);
+        WebView wvSubscribe = (WebView) findViewById(R.id.wvSubscribe);
         WebSettings wsText = wvSubscribe.getSettings();
         wsText.setJavaScriptEnabled(true);
-        wvSubscribe.setWebChromeClient(new WebChromeClient());
-        wvSubscribe.loadUrl("https://tlbinfo.cims.tw/pages/guan-yu-chan-pin");
+        wsText.setDomStorageEnabled(true);
+        wvSubscribe.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.i("TAG",url);
+            }
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        wvSubscribe.loadUrl("https://tlbinfo.cims.tw/");
     }
 }
