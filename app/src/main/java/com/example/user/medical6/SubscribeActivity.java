90debc4d06d72c96f7c27091c4a3cf0cb75f14bf
devilheaven <f011368@gmail.com>
2018-12-27 21:59:49 +0800

    [冠志]subscribe           1.add zoom button           2.press back button return back page

--- app/src/main/java/com/example/user/medical6/SubscribeActivity.java
+++ app/src/main/java/com/example/user/medical6/SubscribeActivity.java
@@ -1,34 +1,44 @@
 package com.example.user.medical6;
 
 import android.net.http.SslError;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+//import android.util.Log;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class SubscribeActivity extends AppCompatActivity {
+    WebView wvSubscribe;
+    WebSettings wsText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
-        WebView wvSubscribe = (WebView) findViewById(R.id.wvSubscribe);
-        WebSettings wsText = wvSubscribe.getSettings();
+        wvSubscribe = (WebView) findViewById(R.id.wvSubscribe);
+        wsText = wvSubscribe.getSettings();
         wsText.setJavaScriptEnabled(true);
-        wsText.setDomStorageEnabled(true);
+        wsText.setBuiltInZoomControls(true);
         wvSubscribe.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                Log.i("TAG",url);
-            }
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                Log.i("TAG",url);
+//            }
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
                 handler.proceed();
             }
         });
         wvSubscribe.loadUrl("https://tlbinfo.cims.tw/");
+    }
+    @Override
+    public void onBackPressed(){
+        if (wvSubscribe.canGoBack()){
+            wvSubscribe.goBack();
+            return;
+        }
+        super.onBackPressed();
     }
 }
