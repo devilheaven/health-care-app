8874e0c119031336fdcf4f192c04439184e2bff7
devilheaven <f011368@gmail.com>
2019-01-06 13:46:54 +0800

    [冠志]1.db create record_status

--- app/src/main/java/com/example/user/medical6/dataBase.java
+++ app/src/main/java/com/example/user/medical6/dataBase.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 class dataBase extends SQLiteOpenHelper{
     //資料庫版本
-    private final static int DATABASE_VERSION = 4;
+    private final static int DATABASE_VERSION = 5;
     //資料表名稱
     public static final String TABLE_c = "customer";
     public static final String TABLE_e = "examine";
@@ -20,12 +20,13 @@ class dataBase extends SQLiteOpenHelper{
     public static final String sex = "gender";
     //檢驗資料表
     public static final String num = "_id";
+    public static final String idnum= "guid";
     public static final String time = "timestamp";
     public static final String weight = "weight";
     public static final String sbp = "sbp";
     public static final String dbp = "dbp";
-    public static final String hr= "hr";
-    public static final String idnum= "guid";
+    public static final String hr = "hr";
+    public static final String record_status = "record_status";
     private final static String DATABASE_NAME = "sql.db";  //資料庫名稱
 
     public dataBase(Context context) {
@@ -36,7 +37,7 @@ class dataBase extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         final String customer = "CREATE TABLE " + TABLE_c + " (" +id + " INTEGER PRIMARY KEY AUTOINCREMENT , "+subjectId+ " VARCHAR(32), "+idnum1+ " VARCHAR(32), " +lastName+ " VARCHAR(32), " + height + " VARCHAR(32), " + sex + " VARCHAR(5));";
         //time TimeStamp NOT NULL DEFAULT (datetime('now','localtime')) 世界時間
-        final String examine = "CREATE TABLE " + TABLE_e  + " (" +num + " INTEGER PRIMARY KEY AUTOINCREMENT , " + time + " TimeStamp , " + weight + " VARCHAR(32), "+ sbp + " VARCHAR(32), "+ dbp + " VARCHAR(32), "+ hr + " VARCHAR(32), " + idnum + " VARCHAR(32));";
+        final String examine = "CREATE TABLE " + TABLE_e  + " (" +num + " INTEGER PRIMARY KEY AUTOINCREMENT , " + time + " TimeStamp , " + weight + " VARCHAR(32), "+ sbp + " VARCHAR(32), "+ dbp + " VARCHAR(32), "+ hr + " VARCHAR(32), " + idnum + " VARCHAR(32)" + record_status+ "VARCHAR(32));";
 
         db.execSQL(customer);
         db.execSQL(examine);
